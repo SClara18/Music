e379b94396b91b6b6113840daad3427fd419d4fe
SClara18 <99375614+SClara18@users.noreply.github.com>
2022-08-11 18:48:49 -0500

    Third commit

--- web/web/settings.py
+++ web/web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+1avu(rol%iy)m61q0_byj!0#m5umt2mhp7k&a@7g9)k)28l%r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
